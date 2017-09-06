006005d85d8d96defd092db0fb8618f4999bed31
Syed Hasnain <syedhasnain.thedevclan@gmail.com>
2017-09-06 20:24:43 +0500

    Side Menu Using third party

---
+++ SideMenu/SideMenu/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Rashdan Natiq on 06/09/2017.
+//  Copyright © 2017 Devclan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var alertButton: UIBarButtonItem!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sideMenu()
+        customizeNavBar()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sideMenu()  {
+        
+        if  revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            alertButton.target = revealViewController()
+            alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+   
+    func customizeNavBar()  {
+        navigationController?.navigationBar.tintColor = UIColor(colorLiteralRed: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(colorLiteralRed: 255/255, green: 87/255, blue: 35/255, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+    }
+}
